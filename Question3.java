086bf32447d32e7c2281485abeb6d80342d8b9f5
MutsaM24020134 <stacymutsa@gmail.com>
2026-09-12 15:27:50 +0200

    Add Question 3 - Even Numbers

---
+++ Question3.java
@@ -0,0 +1,23 @@
+/**
+ * Mutsawashe Maraidza
+ * 24020134
+ * BSc Computer Science and Software Engineering
+ */
+
+public class Question3 {
+    public static void main(String[] args) {
+
+        // Start with the first even number
+        int number = 2;
+
+        // Keep looping while number is 50 or less
+        while (number <= 50) {
+
+            // Print the even number
+            System.out.println(number);
+
+            // Move to the next even number
+            number = number + 2;
+        }
+    }
+}
