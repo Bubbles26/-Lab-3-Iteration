bed45049c32213d03fd2d168b5af750fe58c6e82
MutsaM24020134 <stacymutsa@gmail.com>
2026-09-12 15:07:23 +0200

    Add Question 1 - Countdown

---
+++ Question1.java
@@ -0,0 +1,30 @@
+/**
+ * Mutsawashe Maraidza
+ * 24020134
+ * BSc Computer Science and Software Engineering
+ */
+
+public class Question1 {
+    public static void main(String[] args) {
+
+        // Start the countdown at 10
+        int number = 10;
+
+        // Keep looping while number is 1 or more
+        while (number >= 1) {
+
+            // Print the current number
+            System.out.println(number);
+
+            // Decrease the number by 1
+            number--;
+        }
+
+        // Print Liftoff after the loop ends
+        System.out.println("Liftoff!");
+    }
+}
+
+
+
+
